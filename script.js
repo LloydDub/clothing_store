e6af6c7fea172522d461ac7aa775eece0e3d7a47
Lloyd Worth <lloydrww@gmail.com>
2024-12-09 12:22:58 -0700

    maybe...

--- script.js
+++ script.js
@@ -24,26 +24,3 @@ function updateCart() {
   // Update total price
   totalElement.textContent = `Total: $${total}`;
 }
-(function (w, d, s, o, f, js, fjs) {
-  w["Avease-Widget"] = o;
-  w[o] =
-    w[o] ||
-    function () {
-      (w[o].q = w[o].q || []).push(arguments);
-    };
-  (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-  js.id = o;
-  js.src = f;
-  js.async = 1;
-  fjs.parentNode.insertBefore(js, fjs);
-})(
-  window,
-  document,
-  "script",
-  "av",
-  "https://widget-staging-avease.nexxtdevelopment.ca/assets/snippet.js"
-);
-av("config", {
-  id: "68b2fa0a-f611-412a-83c1-162c8a8fd304",
-});
-av("init");
